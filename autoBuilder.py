4969dcf0893dade33614fa1723b617698d32e21b
Cloud-init-user <debian@database.novalocal>
2018-11-20 00:42:11 +0800

    update

--- autoBuilder.py
+++ autoBuilder.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#/usr/bin/env python
 # -*- coding: utf8 -*-
    
 import pysvn
@@ -9,8 +9,9 @@ import json
 from config import *
 import subprocess
 import sys
-import time
+import time,json,urllib2
 from datetime import datetime
+import requests
 
 def get_login(realm, username, may_save):
     return True, Config.username, Config.password, True
@@ -25,13 +26,15 @@ def get_remote_need_build_version(project,current_version):
     revision = pysvn.Revision(pysvn.opt_revision_kind.number,current_version);
     log_list = client.log(project.get_svn_path(),revision_end = revision);
     validRevision = current_version;
+    lastAuthor = '';
     
     for log in log_list:
         log_revision = log.revision.number;
-        if keyword in log.message and log_revision > validRevision:
+        if keyword in log.message and log_revision >= validRevision:
             print_log('check buildable version:' + str(log_revision) + ',log:' + log.message);
             validRevision = log_revision;
-    return validRevision;
+            lastAuthor = log.author;
+    return validRevision,lastAuthor;
 
 def svn_update(project,target_version):
     try:
@@ -44,13 +47,26 @@ def svn_update(project,target_version):
     except Exception,err:
         print_log('update ' + project.get_name() + ' error:' + str(err));
 
+
 def print_log(msg):
-    dt = time.localtime();
-    now = time.strftime('%Y-%M-%d %H:%m:%S', time.localtime());
+    now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime());
     try:
         if not os.path.exists(Config.log_dir):
             os.makedirs(Config.log_dir);
-        fo = open(Config.log_dir + 'log.txt', 'a');
+
+        #以下部分主要判断当前日志文件是否已过期(即已是新的一天)
+        log_file = Config.log_dir + 'log.txt';
+        if(os.path.exists(log_file)):
+            last_modified_time = time.localtime(os.stat(log_file).st_mtime);
+            last_modified_date = time.strftime('%d', last_modified_time);
+            now_date = time.strftime('%d', time.localtime()) ;
+            if(now_date != last_modified_date):#最后修改文件的日期与当前不一致时，将原日志文件进行转移，保证当前日志文件只包含当天的日志
+                now_date_text = time.strftime('%Y-%m-%d', time.localtime());
+                new_file_path = Config.log_dir + now_date_text + '-log.txt';
+                os.rename(log_file,new_file_path);
+  
+        #记录日志
+        fo = open(log_file, 'a');
         fo.write(now + ' ' + str(msg) + '\n');
         fo.close();
     except Exception,err:
@@ -60,8 +76,8 @@ def print_log(msg):
 
 def check_need_update(project):
     current_version = get_local_version(project);
-    latest_version = get_remote_need_build_version(project,current_version);#仅查询svn上需要build的最新版本号,防止无效获取并build
-    return latest_version > current_version,latest_version;
+    latest_version,last_author = get_remote_need_build_version(project,current_version);#仅查询svn上需要build的最新版本号,防止无效获取并build
+    return latest_version > current_version,latest_version,last_author;
 
 def check_out(project):
     ret = client.checkout(project.get_svn_path(), project.get_local_code_path());
@@ -81,19 +97,59 @@ def ensure_checkout():
                 shutil.rmtree(local_code_dir);#检出失败时，递归删除该目录，方便下次再次检出
                 raise Exception,err;#重新抛出该异常，中断后续操作，保证只有在检出成功的情况下才继续进行        
 
+def check_in(project,log_message):
+    dist_dir = project.get_local_dist_path();
+    project_name = project.get_name();
+    print_log('check in ' + project_name + ' start');
+    try:
+        print_log('check in ' + dist_dir + ' ... ');
+        add_files(dist_dir);
+        client.checkin(dist_dir,log_message);    
+        print_log('check in ' + project_name + ' successfully');
+    except Exception,err:
+        print_log('check out ' + project_name + ' failed:' + str(err));
+
+def add_files(path):
+    changes = client.status(path);
+    for change in changes:
+        if(change.text_status == pysvn.wc_status_kind.unversioned):
+            print_log('add file:' + change.path);
+            client.add(change.path);
+
+def send_dingtalk_msg(msg,at):
+    url = Config.robot_url;
+    if Config.robot_url:
+       data = {'msgtype' : 'text' , 'text' : { 'content' : msg }, 'at' : { 'atMobiles' : at }};
+       # r = requests.post(url,json.dumps(data));
+       json_dump = json.dumps(data);
+       req_con = urllib2.Request(url,json_dump)
+       req_con.add_header('Content-Type', 'application/json')
+       response = urllib2.urlopen(req_con)
+       print(response);
+
 def auto_update():
     while True:
         for project in Config.projects:
             project_name = project.get_name();
 	    try:
-                result = check_need_update(project);
-                need_update = result[0];
-                version = result[1];
-                if(need_update):
-                    print_log(project_name +' new version detected:' + str(version));
+                need_update,version,last_author = check_need_update(project);
+                if(need_update or True):
+                    print_log(project_name +' new version detected:' + str(version) + ' by ' + last_author);
                     svn_update(project,version);#更新该目录至指定svn版本
-                    build(project);#build 该项目
+                    try:                
+                        target_cellphone = [];
+                        phone = Config.get_cellphone_by_name(last_author);
+                        if(phone):
+                            target_cellphone = [phone];
+                        send_dingtalk_msg('项目 ' + project_name + ' 收到你的build请求, 版本号:' + str(version) + ',正在开始build...',target_cellphone);
+                        build(project);#build 该项目
+                        send_dingtalk_msg('项目 ' + project_name + ' 版本号:' + str(version) + ',build 成功！',target_cellphone);
+                    except Exception,err:
+                        print_log(str(err));
+                        send_dingtalk_msg('项目 ' + project_name + ' 版本号:' + str(version) + ',build 失败！详情请查看:https://t.xls.kuaidiantong.cn/autoBuilder/log',target_cellphone);
+                        raise;
                     copy_to_dist(project);#复制生成后的文件到发布目录，主要防止生成时间过长造成发布目录访问受影响
+                    check_in(project , 'auto building from version ' + str(version) + ' by ' + last_author);
                 else:
                     print_log(project_name + ' no avaliable version!');
             except Exception,err:
@@ -104,7 +160,7 @@ def build(project):
     project_name = project.get_name();
     try:
         print_log('build project ' + project.get_name() + ' start');
-        scripts = 'npm run build --prefix ' + project.get_local_code_path();
+        scripts = 'npm run build --no-cache --prefix ' + project.get_local_code_path();
         run_shell_scripts(scripts);
         print_log('build project ' + project.get_name() + ' finished');
     except Exception,err:
@@ -153,6 +209,7 @@ def setlocale():
 
 if "__main__" == __name__:
     setlocale();
+    # send_dingtalk_msg('ok ',['18570392802']);
     client = pysvn.Client();
     client.callback_get_login = get_login;
     ensure_checkout();
